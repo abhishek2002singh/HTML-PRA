2b6978d8f7dc85da6c83838b8b0a78c1d043d99e
abhishek sing <singhsabhishek23@gmail.com>
2022-11-11 15:05:23 +0530

    initial commit

---
+++ OneDrive/Desktop/java/decition control/evenodd.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class evenodd {
+    public static void main(String args[]) {
+        Scanner sr=new Scanner(System.in);
+        int number=sr.nextInt();
+        if(number%2==0)
+        {
+            System.out.println("even number");
+
+        }
+        else
+        {
+            System.out.println("odd number");
+        }
+    }
+}
